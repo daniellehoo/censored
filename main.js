0d07343701bd132027ae1ff456cc5f8b43844233
Danielle Hoo <daniellem.hoo@gmail.com>
2020-09-03 23:48:17 -0400

    completed carousel logic and functionality

--- main.js
+++ main.js
@@ -8,26 +8,22 @@ let state = {
   photoId: 0,
 };
 
-function populateCarousel(args) {
+const populateCarousel = (args) => {
   args.map((item, index) => {
     imageArr.push(item);
   });
 }
 
 const handleLeftClick = () => {
-  state.photoId === 0 ? state.photoId++ : (state.photoId = imageArr.length);
-  console.log(state.photoId);
+  state.photoId === 0 ? state.photoId = imageArr.length - 1 : state.photoId--;
   imageContainer.style.backgroundImage =
     'url(' + src + imageArr[state.photoId] + ')';
-  console.log(imageContainer.style);
 };
 
 const handleRightClick = () => {
-  state.photoId < imageArr.length ? state.photoId++ : (state.photoId = 0);
+  state.photoId < imageArr.length - 1 ? state.photoId++ : state.photoId = 0;
   imageContainer.style.backgroundImage =
     'url(' + src + imageArr[state.photoId] + ')';
-      console.log(imageContainer.style.backgroundImage);
-      console.log(state.photoId)
 };
 
 window.onload = function () {
@@ -35,8 +31,3 @@ window.onload = function () {
   leftButton.addEventListener('click', handleLeftClick);
   rightButton.addEventListener('click', handleRightClick);
 };
-
-// create array of image srcs + ids
-// set base case of state
-// add 'onclick' functionality to each button
-// update state according to id number of image
