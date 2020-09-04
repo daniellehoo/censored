b95ceaf3ebe79909439d39acb835ca85f469a028
Danielle Hoo <daniellem.hoo@gmail.com>
2020-09-04 14:48:58 -0400

    completed circle nav logic

--- main.js
+++ main.js
@@ -3,31 +3,56 @@ let imageContainer = document.getElementById('image-container');
 let leftButton = document.getElementById('left-arrow');
 let rightButton = document.getElementById('right-arrow');
 let imageArr = [];
-
+let circles = document.getElementsByClassName('circle-container')[0];
 let state = {
   photoId: 0,
 };
 
 const populateCarousel = (args) => {
   args.map((item, index) => {
+    let circle = document.createElement('div');
+    circle.id = index;
+    circle.classList.add('circle');
+    circles.append(circle);
     imageArr.push(item);
   });
-}
+};
+
+const toggleCircles = () => {
+  let circleCollection = document.getElementsByClassName('circle');
+  let circleArr = [...circleCollection];
+  if (circleArr.length > 0) {
+    circleArr.map((item, index) => {
+      state.photoId === index
+        ? circleArr[index].classList.add('active')
+        : circleArr[index].classList.remove('active');
+      item.addEventListener('click', () => {
+        state.photoId = index;
+        imageContainer.style.backgroundImage =
+          'url(' + src + imageArr[state.photoId] + ')';
+        toggleCircles();
+      });
+    });
+  }
+};
 
 const handleLeftClick = () => {
-  state.photoId === 0 ? state.photoId = imageArr.length - 1 : state.photoId--;
+  state.photoId === 0 ? (state.photoId = imageArr.length - 1) : state.photoId--;
   imageContainer.style.backgroundImage =
     'url(' + src + imageArr[state.photoId] + ')';
+  toggleCircles();
 };
 
 const handleRightClick = () => {
-  state.photoId < imageArr.length - 1 ? state.photoId++ : state.photoId = 0;
+  state.photoId < imageArr.length - 1 ? state.photoId++ : (state.photoId = 0);
   imageContainer.style.backgroundImage =
     'url(' + src + imageArr[state.photoId] + ')';
+  toggleCircles();
 };
 
 window.onload = function () {
-  populateCarousel(['ump.jpg', 'ump1.jpg', 'mel.jpg', 'ivanka.jpg']);
+  populateCarousel(['ump.jpg', 'mel.jpg', 'ivanka.jpg', 'dtj.jpg']);
+  toggleCircles();
   leftButton.addEventListener('click', handleLeftClick);
   rightButton.addEventListener('click', handleRightClick);
 };
